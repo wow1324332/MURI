f860542714f2c0c7acece67f7a1e28ee7afe67de
wow1324332 <wow1324332@gmail.com>
2026-05-15 21:44:39 +0900

    Configure build target to 'esnext' in vite.config.js
    Add build target configuration for modern browsers.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    target: 'esnext' // 모던 브라우저 지원을 위해 ESNext 타겟팅
+  }
 })
